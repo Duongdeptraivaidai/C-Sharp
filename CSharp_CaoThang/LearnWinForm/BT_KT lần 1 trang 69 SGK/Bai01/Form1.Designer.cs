f13242418b11ad9dbb35da54bb7759e517fd71eb
Duong Nguyen <dathuynh594@gmail.com>
2026-03-05 14:40:15 +0700

    Add files via upload

---
+++ CSharp_CaoThang/LearnWinForm/BT_KT lần 1 trang 69 SGK/Bai01/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Bai01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_SecurityCode = new System.Windows.Forms.TextBox();
+            this.btn_Num1 = new System.Windows.Forms.Button();
+            this.btn_Num2 = new System.Windows.Forms.Button();
+            this.btn_Num3 = new System.Windows.Forms.Button();
+            this.btn_Num4 = new System.Windows.Forms.Button();
+            this.btn_Num5 = new System.Windows.Forms.Button();
+            this.btn_Num6 = new System.Windows.Forms.Button();
+            this.btn_Num7 = new System.Windows.Forms.Button();
+            this.btn_Num8 = new System.Windows.Forms.Button();
+            this.btn_Num9 = new System.Windows.Forms.Button();
+            this.btn_Num0 = new System.Windows.Forms.Button();
+            this.btn_CheckCode = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lst_AccessLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Security";
+            // 
+            // txt_SecurityCode
+            // 
+            this.txt_SecurityCode.Location = new System.Drawing.Point(147, 31);
+            this.txt_SecurityCode.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txt_SecurityCode.Name = "txt_SecurityCode";
+            this.txt_SecurityCode.Size = new System.Drawing.Size(228, 20);
+            this.txt_SecurityCode.TabIndex = 1;
+            // 
+            // btn_Num1
+            // 
+            this.btn_Num1.Location = new System.Drawing.Point(145, 66);
+            this.btn_Num1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Num1.Name = "btn_Num1";
+            this.btn_Num1.Size = new System.Drawing.Size(50, 23);
+            this.btn_Num1.TabIndex = 2;
+            this.btn_Num1.Text = "1";
+            this.btn_Num1.UseVisualStyleBackColor = true;
+            // 
+            // btn_Num2
+            // 
+            this.btn_Num2.Location = new System.Drawing.Point(202, 66);
+            this.btn_Num2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Num2.Name = "btn_Num2";
+            this.btn_Num2.Size = new System.Drawing.Size(50, 23);
+            this.btn_Num2.TabIndex = 3;
+            this.btn_Num2.Text = "2";
+            this.btn_Num2.UseVisualStyleBackColor = true;
+            // 
+            // btn_Num3
+            // 
+            this.btn_Num3.Location = new System.Drawing.Point(262, 66);
+            this.btn_Num3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Num3.Name = "btn_Num3";
+            this.btn_Num3.Size = new System.Drawing.Size(50, 23);
+            this.btn_Num3.TabIndex = 4;
+            this.btn_Num3.Text = "3";
+            this.btn_Num3.UseVisualStyleBackColor = true;
+            // 
+            // btn_Num4
+            // 
+            this.btn_Num4.Location = new System.Drawing.Point(145, 107);
+            this.btn_Num4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Num4.Name = "btn_Num4";
+            this.btn_Num4.Size = new System.Drawing.Size(50, 23);
+            this.btn_Num4.TabIndex = 5;
+            this.btn_Num4.Text = "4";
+            this.btn_Num4.UseVisualStyleBackColor = true;
+            // 
+            // btn_Num5
+            // 
+            this.btn_Num5.Location = new System.Drawing.Point(202, 107);
+            this.btn_Num5.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Num5.Name = "btn_Num5";
+            this.btn_Num5.Size = new System.Drawing.Size(50, 23);
+            this.btn_Num5.TabIndex = 6;
+            this.btn_Num5.Text = "5";
+            this.btn_Num5.UseVisualStyleBackColor = true;
+            // 
+            // btn_Num6
+            // 
+            this.btn_Num6.Location = new System.Drawing.Point(261, 107);
+            this.btn_Num6.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Num6.Name = "btn_Num6";
+            this.btn_Num6.Size = new System.Drawing.Size(50, 23);
+            this.btn_Num6.TabIndex = 7;
+            this.btn_Num6.Text = "6";
+            this.btn_Num6.UseVisualStyleBackColor = true;
+            // 
+            // btn_Num7
+            // 
+            this.btn_Num7.Location = new System.Drawing.Point(145, 148);
+            this.btn_Num7.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Num7.Name = "btn_Num7";
+            this.btn_Num7.Size = new System.Drawing.Size(50, 23);
+            this.btn_Num7.TabIndex = 8;
+            this.btn_Num7.Text = "7";
+            this.btn_Num7.UseVisualStyleBackColor = true;
+            // 
+            // btn_Num8
+            // 
+            this.btn_Num8.Location = new System.Drawing.Point(202, 148);
+            this.btn_Num8.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Num8.Name = "btn_Num8";
+            this.btn_Num8.Size = new System.Drawing.Size(50, 23);
+            this.btn_Num8.TabIndex = 9;
+            this.btn_Num8.Text = "8";
+            this.btn_Num8.UseVisualStyleBackColor = true;
+            // 
+            // btn_Num9
+            // 
+            this.btn_Num9.Location = new System.Drawing.Point(262, 148);
+            this.btn_Num9.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Num9.Name = "btn_Num9";
+            this.btn_Num9.Size = new System.Drawing.Size(50, 23);
+            this.btn_Num9.TabIndex = 10;
+            this.btn_Num9.Text = "9";
+            this.btn_Num9.UseVisualStyleBackColor = true;
+            // 
+            // btn_Num0
+            // 
+            this.btn_Num0.Location = new System.Drawing.Point(319, 148);
+            this.btn_Num0.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Num0.Name = "btn_Num0";
+            this.btn_Num0.Size = new System.Drawing.Size(56, 23);
+            this.btn_Num0.TabIndex = 11;
+            this.btn_Num0.Text = "0";
+            this.btn_Num0.UseVisualStyleBackColor = true;
+            // 
+            // btn_CheckCode
+            // 
+            this.btn_CheckCode.Location = new System.Drawing.Point(319, 107);
+            this.btn_CheckCode.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_CheckCode.Name = "btn_CheckCode";
+            this.btn_CheckCode.Size = new System.Drawing.Size(56, 23);
+            this.btn_CheckCode.TabIndex = 12;
+            this.btn_CheckCode.Text = "Check";
+            this.btn_CheckCode.UseVisualStyleBackColor = true;
+            this.btn_CheckCode.Click += new System.EventHandler(this.btn_CheckCode_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(319, 66);
+            this.btn_Clear.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(56, 23);
+            this.btn_Clear.TabIndex = 13;
+            this.btn_Clear.Text = "C";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 179);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Access log:";
+            // 
+            // lst_AccessLog
+            // 
+            this.lst_AccessLog.FormattingEnabled = true;
+            this.lst_AccessLog.Location = new System.Drawing.Point(30, 195);
+            this.lst_AccessLog.Name = "lst_AccessLog";
+            this.lst_AccessLog.Size = new System.Drawing.Size(345, 95);
+            this.lst_AccessLog.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 333);
+            this.Controls.Add(this.lst_AccessLog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_CheckCode);
+            this.Controls.Add(this.btn_Num0);
+            this.Controls.Add(this.btn_Num9);
+            this.Controls.Add(this.btn_Num8);
+            this.Controls.Add(this.btn_Num7);
+            this.Controls.Add(this.btn_Num6);
+            this.Controls.Add(this.btn_Num5);
+            this.Controls.Add(this.btn_Num4);
+            this.Controls.Add(this.btn_Num3);
+            this.Controls.Add(this.btn_Num2);
+            this.Controls.Add(this.btn_Num1);
+            this.Controls.Add(this.txt_SecurityCode);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Security Panel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_SecurityCode;
+        private System.Windows.Forms.Button btn_Num1;
+        private System.Windows.Forms.Button btn_Num2;
+        private System.Windows.Forms.Button btn_Num3;
+        private System.Windows.Forms.Button btn_Num4;
+        private System.Windows.Forms.Button btn_Num5;
+        private System.Windows.Forms.Button btn_Num6;
+        private System.Windows.Forms.Button btn_Num7;
+        private System.Windows.Forms.Button btn_Num8;
+        private System.Windows.Forms.Button btn_Num9;
+        private System.Windows.Forms.Button btn_Num0;
+        private System.Windows.Forms.Button btn_CheckCode;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lst_AccessLog;
+    }
+}
+
---
+++ CSharp_CaoThang/LearnWinForm/BT_KT lần 1 trang 69 SGK/Bai01/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Bai01
+{
+    public partial class Form1 : Form
+    {
+        // 1. Dùng Dictionary để quản lý mã code và nhóm nhân viên cho dễ mở rộng
+        private readonly Dictionary<string, string> securityCodes = new Dictionary<string, string>
+        {
+            { "1645", "Technicians" }, { "1689", "Technicians" },
+            { "8345", "Custodians" },
+            { "9998", "Scientist" }, { "1006", "Scientist" }, { "1008", "Scientist" }
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupNumberButtons();
+        }
+
+        // 2. Tự động gán sự kiện cho các nút số 0-9
+        private void SetupNumberButtons()
+        {
+            foreach (Control c in this.Controls)
+            {
+                // Kiểm tra nếu là Button và Text chỉ là 1 ký tự số
+                if (c is Button btn && btn.Text.Length == 1 && char.IsDigit(btn.Text[0]))
+                {
+                    btn.Click += NumberButton_Click;
+                }
+            }
+        }
+
+        // 3. Xử lý nhập số vào ô Security Code
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            if (sender is Button btn && txt_SecurityCode.Text.Length < 4)
+            {
+                txt_SecurityCode.Text += btn.Text;
+            }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txt_SecurityCode.Clear();
+            txt_SecurityCode.BackColor = Color.White;
+        }
+
+        // 4. Logic kiểm tra mã và ghi log theo yêu cầu tài liệu
+        private void btn_CheckCode_Click(object sender, EventArgs e)
+        {
+            string input = txt_SecurityCode.Text;
+            string time = DateTime.Now.ToString("dd/MM/yyyy h:mm tt");
+            string status;
+
+            // Kiểm tra các trường hợp đặc biệt theo yêu cầu
+            if (string.IsNullOrEmpty(input)) return;
+
+            if (securityCodes.ContainsKey(input))
+            {
+                // Trạng thái Granted: Hiển thị nhóm nhân viên
+                status = securityCodes[input];
+                txt_SecurityCode.BackColor = Color.LightGreen;
+            }
+            else if (input.Length == 1)
+            {
+                // Nhấn 1 con số: Hiển thị Restricted Access
+                status = "Restricted Access!";
+                txt_SecurityCode.BackColor = Color.Orange;
+            }
+            else
+            {
+                // Sai mã: Hiển thị Access denied
+                status = "Access denied";
+                txt_SecurityCode.BackColor = Color.Salmon;
+            }
+
+            // Ghi nhật ký vào ListBox (Tin mới nhất lên đầu)
+            lst_AccessLog.Items.Insert(0, $"{time}    {status}");
+
+            // Tự động reset sau 1 giây để đón lượt nhập tiếp theo
+            ResetAfterDelay();
+        }
+
+        private void ResetAfterDelay()
+        {
+            Timer timer = new Timer { Interval = 1000 };
+            timer.Tick += (s, ev) => {
+                txt_SecurityCode.Clear();
+                txt_SecurityCode.BackColor = Color.White;
+                timer.Stop();
+                timer.Dispose();
+            };
+            timer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Chuyển ký tự hiển thị thành dấu *
+            txt_SecurityCode.PasswordChar = '*';
+
+            // Ngăn người dùng nhập từ bàn phím máy tính, chỉ cho phép bấm nút trên màn hình
+            txt_SecurityCode.ReadOnly = true;
+        }
+    }
+}
